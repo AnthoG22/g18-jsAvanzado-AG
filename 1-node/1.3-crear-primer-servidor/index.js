b504faa21dda9f6b9e94171f0b5fc3b88e79ab16
AnthoG22 <gonzalesanthony22@gmail.com>
2022-09-26 19:06:47 -0500

    second commit

--- 1-node/1.3-crear-primer-servidor/index.js
+++ 1-node/1.3-crear-primer-servidor/index.js
@@ -1,11 +1,12 @@
 const http = require('http');
 
-const server = http.createServer((request,response)=>{
-    const mymeType = { 'content-type' : 'text/html'}
-
-    response.write('<html><body>hola desde mi primer servidor</body> </html>');
+const server = http.createServer((request, response) => {
+    const status = 200; // OK
+    const mymeType = { 'Content-type': 'text/html' };
+    response.writeHead(status, mymeType);
+    response.write('<html> <body> hola desde mi primer servidor! </body> </html>');
     response.end();
-})
+});
 
 server.listen(8080);
 console.log('servidor ejecutandose...');
